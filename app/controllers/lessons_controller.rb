ec9e5d3e7e1cdcced6025a35854293033a39bb7c
github Ryuya-web <mofa196@mbox.re>
2020-05-18 09:19:29 +0900

    sixth commit

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -33,6 +33,9 @@ class LessonsController < ApplicationController
         @lessons = Lesson.where(title:@lesson.title).order("created_at ASC") 
         @all_ranks = Lesson.find(Like.group(:lesson_id).order('count(lesson_id) desc').limit(50).distinct.pluck(:lesson_id))
     end
+    def ranking
+        
+    end
     private
     def lesson_params
         params.require(:lesson).permit(:teacher_name, :title, :body)
