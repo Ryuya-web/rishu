2975ff49477cfc85471f78368959cb6872fc439a
github Ryuya-web <mofa196@mbox.re>
2020-05-22 16:57:15 +0900

    heroku19

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_22_042304) do
+ActiveRecord::Schema.define(version: 2020_05_22_074212) do
 
   create_table "lessonbs", force: :cascade do |t|
     t.string "title"
@@ -210,14 +210,7 @@ ActiveRecord::Schema.define(version: 2020_05_22_042304) do
     t.index ["user_id"], name: "index_likesyos_on_user_id"
   end
 
-  create_table "teachers", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.string "name"
-  end
-
   create_table "users", force: :cascade do |t|
-    t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
